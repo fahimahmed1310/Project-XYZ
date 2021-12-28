063aa36713711466afbc5a21e0ea19064f202e0f
Fahim Ahmed <fahimahmed8798740@gmail.com>
2021-12-29 01:57:28 +0600

    Color changed(scaffold)

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() {
   runApp( MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Scaffold(
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.teal,
       appBar: AppBar(
         backgroundColor: Colors.green,
         leading: Image(
